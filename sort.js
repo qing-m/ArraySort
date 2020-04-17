924fc376e0545c7c6ed542a1cddd4a003644cd78
wangheyao <wangheyao@zealfi.com>
2020-04-17 13:25:35 +0800

    fix

--- sort.js
+++ sort.js
@@ -27,3 +27,23 @@ for(let i=0;i<=Arr2.length;i++) {
 	}
 }
 console.log(Arr2)
+
+
+// 选择排序
+let Arr3 = Arr
+function selectionSort(Arr3) {
+	var len = Arr3.length;
+	var minIndex, temp;
+	for (var i = 0; i < len - 1; i++) {
+			minIndex = i;
+			for (var j = i + 1; j < len; j++) {
+					if (Arr3[j] < Arr3[minIndex]) {     //寻找最小的数
+							minIndex = j;                 //将最小数的索引保存
+					}
+			}
+			temp = Arr3[i];
+			Arr3[i] = Arr3[minIndex];
+			Arr3[minIndex] = temp;
+	}
+	return arr;
+}
